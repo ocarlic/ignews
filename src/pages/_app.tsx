4a8b402ef634e70567eb886e4be9aa6fb64ab4fd
Victor Carli <victorcarlibr@gmail.com>
2021-08-16 23:53:46 -0300

    config document and global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
-    <h1 className={styles.title}>test</h1>
+    <h1>Home page</h1>
   )
 }
