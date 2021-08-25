c2077984218b633a5b7fc8251028e1971281cb35
Victor Carli <victorcarlibr@gmail.com>
2021-08-25 20:39:30 -0300

    component home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import Image from 'next/image';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Home page</h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <Image 
+        src="/images/avatar.svg" 
+        alt="Girl coding"
+        width={80}
+        height={80}
+      />
+    </main>
   )
 }
