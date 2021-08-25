0703a67a821c5c4072982dfa1167b36d96e6468b
Victor Carli <victorcarlibr@gmail.com>
2021-08-25 20:52:32 -0300

    component SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -12,13 +13,14 @@ export default function Home() {
           Get access to all the publications <br />
           <span>for $9.90 month</span>
         </p>
+        <SubscribeButton />
       </section>
 
       <Image 
         src="/images/avatar.svg" 
         alt="Girl coding"
-        width={80}
-        height={80}
+        width={336}
+        height={521}
       />
     </main>
   )
