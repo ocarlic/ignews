bc93a78594690704dcbf511b474da0b7c8e4e11b
Victor Carli <victorcarlibr@gmail.com>
2021-09-26 00:07:20 -0300

    add button signInButton with auth github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,20 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import { Header } from '../components/Header';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Head>
         <title>ig.news</title>
       </Head>
       
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
